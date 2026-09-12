96cc99527198bdd6fd8eb409caa7dd08291e0d7d
bbelu <belensoares@gmail.com>
2026-09-12 18:21:55 -0300

    js

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,13 +1,19 @@
-let cantidad = document.querySelector("#cantidad");
-let consumoPorHora = document.querySelector("#consumoPorHora");
-let costoPorKwh = document.querySelector("#costoPorKwh");
+// Formulario inicial
+let inputCantidad = document.querySelector("#cantidad");
+let inputConsumoPorHora = document.querySelector("#consumoPorHora");
+let inputCostoPorKwh = document.querySelector("#costoPorKwh");
 let btnIniciar = document.querySelector("#btnIniciar");
-let nombreObra = document.querySelector("#nombreObra");
-let cantidadLuces = document.querySelector("#cantidadLuces");
-let tiempoLuces = document.querySelector("#tiempoLuces");
+
+// Formulario por obra
+let inputNombreObra = document.querySelector("#nombreObra");
+let inputCantidadLuces = document.querySelector("#cantidadLuces");
+let inputTiempoLuces = document.querySelector("#tiempoLuces");
 let btnAgregarObra = document.querySelector("#btnAgregarObra");
+
+// Resultados
 let btnCalcular = document.querySelector("#btnCalcular");
 let consumoTotal = document.querySelector("#consumoTotal");
 let consumoPromedio = document.querySelector("#consumoPromedio");
 let obraMaxTiempo = document.querySelector("#obraMaxTiempo");
 let porcentajeLuces = document.querySelector("#porcentajeLuces");
+let btnReiniciar = document.querySelector("#btnReiniciar");
